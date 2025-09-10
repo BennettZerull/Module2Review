e1ff94e8bb46e2c4af8d90a0054f43185c3f01a1
Bennett Zerull <zerullbe@gmail.com>
2025-09-10 17:16:29 -0500

    Compleated step 5

--- Student Grade Calculator/Program.cs
+++ Student Grade Calculator/Program.cs
@@ -10,6 +10,20 @@ namespace Student_Grade_Calculator
         {
             var students = new List<Student>();
             //Step Four: I created a list for students in the main method
+            Student student1 = new Student();
+            Student student2 = new Student();
+            Student student3 = new Student();
+            Student student4 = new Student();
+            
+            student1.name = "Bennett Zerull";
+            student1.id = 001;
+            student2.name = "Wedge Mydoggo";
+            student2.id = 002;
+            student3.name = "Mario Brother";
+            student3.id = 003;
+            student4.name = "Luigi Brother";
+            student4.id = 004;
+            //Step Five: I created 4 instances of students
         }
     }
 }
