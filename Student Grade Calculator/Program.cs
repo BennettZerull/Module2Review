56ef3ffeed7eb08c62ec4a1f5cc8d7a82b366218
Bennett Zerull <zerullbe@gmail.com>
2025-09-10 15:24:39 -0500

    compleated steps 1 and 2 of the module review

---
+++ Student Grade Calculator/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Step One: I created the console application to calculate students grades.
+Console.WriteLine("Hello, World!");
---
+++ Student Grade Calculator/Student.cs
@@ -0,0 +1,9 @@
+namespace Student_Grade_Calculator;
+
+public class Student
+{
+    //Step Two: I created a class for students and added properties to the class.
+    string name;
+    int id;
+    List<double> grades = new List<double>();
+}
